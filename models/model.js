3daf9e89845dc1cc56cf46a2c768f745eb6583bc
justinpeake <justin@articulatedworks.com>
2016-01-20 17:18:53 -0500

    forfone

--- models/model.js
+++ models/model.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 // See http://mongoosejs.com/docs/schematypes.html
 
 var figureSchema = new Schema({
-	name: String,
+	figureName: String,
 	// name: {type: String, required: true}, // this version requires this field to exist
 	// name: {type: String, unique: true}, // this version requires this field to be unique in the db
 	keySig: String,
@@ -12,17 +12,17 @@ var figureSchema = new Schema({
 	//{ collection: 'Figures'}
 })
 
-var userSchema = new Schema({
-	name: String,
-	// name: {type: String, required: true}, // this version requires this field to exist
-	// name: {type: String, unique: true}, // this version requires this field to be unique in the db
-	password: String,
-	comps: [String],
-	url: String,
-	dateAdded : { type: Date, default: Date.now },
-})
+// var userSchema = new Schema({
+// 	name: String,
+// 	// name: {type: String, required: true}, // this version requires this field to exist
+// 	// name: {type: String, unique: true}, // this version requires this field to be unique in the db
+// 	password: String,
+// 	comps: [String],
+// 	url: String,
+// 	dateAdded : { type: Date, default: Date.now },
+// })
 
 // export 'Animal' model so we can interact with it in other files
 module.exports = mongoose.model('Figure',figureSchema);
 
-module.exports = mongoose.model('User',userSchema);
+//module.exports = mongoose.model('User',userSchema);
