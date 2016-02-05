d44b3eaf6c6ee95f81c1add323fed3a3e7f0982d
justinpeake <justin@articulatedworks.com>
2016-02-04 21:15:40 -0500

    rewrite register

--- routes/index.js
+++ routes/index.js
@@ -79,7 +79,7 @@ router.get('/login', function(req, res) {
 //TEST NEW Save COnfig
 router.post('/register', function(req, res) {
 
-    var account = new Account({ username : req.body.username }), req.body.password, function(err, account) {
+    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
        
         if (err) {
           return res.render("register.html", {info: "Sorry. That username already exists. Try again."});
