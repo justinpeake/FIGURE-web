a7bd69d23017d3b6363d9ff886178b4265f63a17
justinpeake <justin@articulatedworks.com>
2016-02-06 00:29:08 -0500

    awsstuff

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ var routes = require('./routes/index.js');
 var users = require('./routes/users.js');
 
  
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET
+
 app.io = io;  //second iteration
 
 
@@ -77,6 +81,44 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+
+//AWS S3 SHIZ
+
+app.get('/sign_s3', function(req, res){
+    aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+    var s3 = new aws.S3();
+    var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: req.query.file_name,
+        Expires: 60,
+        ContentType: req.query.file_type,
+        ACL: 'public-read'
+    };
+    s3.getSignedUrl('putObject', s3_params, function(err, data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var return_data = {
+                signed_request: data,
+                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+            };
+            res.write(JSON.stringify(return_data));
+            res.end();
+        }
+    });
+});
+
+app.post('/submit_form', function(req, res){
+    username = req.body.username;
+    full_name = req.body.full_name;
+    avatar_url = req.body.avatar_url;
+    update_account(username, full_name, avatar_url); // TODO: create this function
+    // TODO: Return something useful or redirect
+});
+
+// END AWS S3 SHIZ
+
 // error handlers
 
 // development error handler
@@ -102,6 +144,8 @@ app.use(function(err, req, res, next) {
 });
 
 
+
+//do not erase this 
 var performerCount = 0;
 
 
