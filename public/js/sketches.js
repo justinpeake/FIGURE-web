a0b04ab4fe521bbeebed95d87c50c5ebe372107e
justinpeake <justin@articulatedworks.com>
2016-03-28 22:27:26 -0400

    working update

--- public/js/sketches.js
+++ public/js/sketches.js
@@ -99,104 +99,102 @@ var tacet= function( p ) {
 
 var waveForm = function(p){
 
-var track;
-var peaks=[];
-var canvasX = 700;
-var canvasY = 400;
-var x = 0;
-var y = 200;
-var wavHeight = 200;
-var wavLength = canvasX;
-var thisNum = 0;
+  var track;
+  var peaks=[];
+  var canvasX = 700;
+  var canvasY = 400;
+  var x = 0;
+  var y = 200;
+  var wavHeight = 200;
+  var wavLength = canvasX;
+
+  var thisNum = 0;
 
 
 
-p.preload = function(){
+  p.preload = function(){
 
-  track = p.loadSound(trackName);
-
-}
-
-p.setup = function() {
-
-  p.createCanvas(canvasX,canvasY);
-  p.background(0);
-
-  track.setVolume(0);
-  track.loop();
-  
-  peaks = track.getPeaks(wavLength);
-  
-    // draw the unplayed waveform
-    for(i=0; i < peaks.length; i++){
-      p.stroke(255);
-      p.strokeWeight(1);
-      p.line(x, y + (wavHeight * peaks[i]),x, y - (wavHeight * peaks[i]))
-      x = x + 1;
-    };
-    
-    //reset x for playback
-    x = 0;
-}; // end setup
-
-
-p.draw = function(){
-
-  // map the length of song to the amount of samples in peake.length  
-thisNum = p.map(track.currentTime(), 0, track.duration(), 0, peaks.length)
-intNum = p.int(thisNum);
-
-
-  p.stroke(255);
-  p.strokeWeight(2);
-  p.fill(0);
-
- if (track.isPlaying()){
-
-  p.fill(0);
-  p.noStroke();
-  p.rect(x,0, 2, canvasY);
-  
-  p.fill(255,255,0);
-  p.stroke(0,255,255);
-  p.line(x, y + (wavHeight * peaks[intNum]),x, y - (wavHeight * peaks[intNum]));
- //x = x + 1;
-  x = intNum;
- }
- 
- if (x == 0){
-   p.waveRedraw();
- }
- 
-  p.fill(0);
-  p.noStroke();
-  p.fill(255);
- 
-}
-
-p.waveRedraw = function(){
-  
-   for(i=0; i < peaks.length; i++){
-    
-      p.stroke(255);
-      p.strokeWeight(1);
-      p.line(x, y + (wavHeight * peaks[i]),x, y - (wavHeight * peaks[i]))
-      x = x + 1;
-    };
-    //reset x for playback
-    x = 0;
-}
-
-p.waveRestart = function() {
-
-    console.log('restart');
-    console.log(trackName);
-   // track = p.loadSound(trackName);
+    track = p.loadSound(trackName);
 
   }
 
+  p.setup = function() {
+
+    p.createCanvas(canvasX,canvasY);
+    p.background(0);
+
+    track.setVolume(0);
+    track.loop();
+    
+    peaks = track.getPeaks(wavLength);
+    
+      // draw the unplayed waveform
+      for(i=0; i < peaks.length; i++){
+        p.stroke(255);
+        p.strokeWeight(1);
+        p.line(x, y + (wavHeight * peaks[i]),x, y - (wavHeight * peaks[i]))
+        x = x + 1;
+      };
+      
+      //reset x for playback
+      x = 0;
+  }; // end setup
 
 
+  p.draw = function(){
+
+    // map the length of song to the amount of samples in peake.length  
+  thisNum = p.map(track.currentTime(), 0, track.duration(), 0, peaks.length)
+  intNum = p.int(thisNum);
+
+
+    p.stroke(255);
+    p.strokeWeight(2);
+    p.fill(0);
+
+   if (track.isPlaying()){
+
+    p.fill(0);
+    p.noStroke();
+    p.rect(x,0, 2, canvasY);
+    
+    p.fill(255,255,0);
+    p.stroke(0,255,255);
+    p.line(x, y + (wavHeight * peaks[intNum]),x, y - (wavHeight * peaks[intNum]));
+   //x = x + 1;
+    x = intNum;
+   }
+   
+   if (x == 0){
+     p.waveRedraw();
+   }
+   
+    p.fill(0);
+    p.noStroke();
+    p.fill(255);
+   
+  }
+
+  p.waveRedraw = function(){
+    
+     for(i=0; i < peaks.length; i++){
+      
+        p.stroke(255);
+        p.strokeWeight(1);
+        p.line(x, y + (wavHeight * peaks[i]),x, y - (wavHeight * peaks[i]))
+        x = x + 1;
+      };
+      //reset x for playback
+      x = 0;
+  }
+
+  p.waveRestart = function() {
+
+      console.log('restart');
+      console.log(trackName);
+     // track = p.loadSound(trackName);
+
+  }
 }; // waveform
 
 
