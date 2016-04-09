16db066765e00a60ba4edf7c86fc5f02882a4791
justinpeake <justin@articulatedworks.com>
2016-04-09 17:00:31 -0400

    whoWhom object working

--- views/(image, player).js
+++ views/(image, player).js
@@ -1,31 +1,33 @@
-(image, player)
+
+// if this player is somewhere  in my players array , take it out!
+// assign this image to that player
+
+// var imgs = [elt,elt,elt,elt];
+// var players = [0,1,2,3,4,5,6,7];
+
+// players = {
+// 	player1:'img1',
+// 	player2:'img2',
+// 	player3:'img3' 
+// 	}
 
 
-if this player is somewhere  in my players array , take it out!
-assign this image to that player
+whoWhat = {};
+perfLength = 9;
+
+group = [2];
+
+
+for (i = 0; i < perfLength; i++){
+	var playerNum = i + 1;
+	whoWhat['player' + playerNum] = ' ';
+};
+
+
+
+	whoWhat['player' + 1] = 'test2'
 
 
 
 
-
-var imgs = [elt,elt,elt,elt];
-var players = [0,1,2,3,4,5,6,7];
-
-
-
-for (var i = 0; i < imgs.length; i++){
-	
-}
-
-
-
-
-function(player,image){
-	players[player] = image
-}
-
-player = 
-{player1:img1,
-player2:img2,
-player3:img1
-}
+console.log(whoWhat);
