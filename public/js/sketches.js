c8b05f965354df86698f30bd2c7313d8c483b98d
justinpeake <justin@articulatedworks.com>
2016-07-06 12:13:18 -0400

    styled index

--- public/js/sketches.js
+++ public/js/sketches.js
@@ -198,6 +198,124 @@ var waveForm = function(p){
 }; // waveform
 
 
+//// login stuff
+var introTitle = function(p){
+
+  var opaL1 = 0, opaL2 = 0, opaL3=0, opaL4=0, opaL5=0;
+  var opaF = 0, opaI = 0, opaG = 0, opaU = 0, opaR = 0, opaE = 0, opaN=0;
+  var lineLength = 0;
+  var img;
+  var imgHeight = 100;
+  var iconOpa = 200;
+
+p.preload = function(){
+  img = p.loadImage('assets/awlogoinvert.png');
+  font = p.loadFont('assets/Elevation.ttf');
+}
+
+p.setup = function() {
+  p.createCanvas(700,160);
+  p.textFont(font);
+}
+
+p.draw=function() {
+  p.background(8,3,54);
+  p.textSize(100);
+  p.noStroke();
+  p.fill(200, opaF);
+  p.text("FIGURE",170,115)
+  p.fill(200, opaN);
+  p.textSize(20);
+  p.text("Login", 200, 200)
+  p.text("Sign-Up", 400, 200)
+  p.strokeWeight(1);
+  p.stroke(190, opaL1);
+  p.line(20, 40, lineLength, 40);
+  p.stroke(170, opaL2);
+  p.line(20, 60, lineLength, 60);
+  p.stroke(170, opaL3);
+  p.line(20, 80, lineLength, 80);
+  p.stroke(170, opaL4);
+  p.line(20, 100, lineLength, 100);
+  p.stroke(170, opaL5);
+  p.line(20, 120, lineLength, 120);
+ 
+
+  opaL1 = opaL1 + 2;
+  opaL2 = opaL1 - 20;
+  opaL3 = opaL2 - 20;
+  opaL4 = opaL3 - 20;
+  opaL5 = opaL4 - 10;
+ 
+  opaF = opaL4 - 100;
+  opaN = opaL4 - 180;
+
+  if (lineLength < 680){
+
+  lineLength = lineLength + 5;
+}
+ 
+
+ p.tint(0,200,0, iconOpa);
+ p.image(img, 305, 25);
+
+ iconOpa = iconOpa - 2;
+  if(iconOpa < 0){
+    iconOpa == 0;
+  }
+
+
+  }
+};
+
+////
+
+var introLogin=function(s){
+
+  var font;
+
+  s.preload = function(){
+    font = s.loadFont('assets/Elevation.ttf');
+  }
+
+  s.setup = function() {
+    s.createCanvas(100,30);
+    s.textFont(font);
+  }
+
+  s.draw = function(){
+    s.background(8,3,54);
+    s.textSize(20);
+    s.fill(200);
+    s.text("Login", 20, 20)
+  }
+
+}
+
+/////
+
+var introSign=function(q){
+
+  var font;
+
+  q.preload = function(){
+    font = q.loadFont('assets/Elevation.ttf');
+  }
+
+  q.setup = function() {
+    q.createCanvas(100,30);
+    q.textFont(font);
+  }
+
+  q.draw = function(){
+    q.background(8,3,54);
+    q.textSize(20);
+    q.fill(200);
+    q.text("Sign-Up", 20, 20)
+  }
+
+};
+
 
 
 
