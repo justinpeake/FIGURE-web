f2d8699e998c34e06c08fe34ad9c2c65d7555c3f
justin peake <>
2019-06-01 12:28:07 +0200

    cleaning up

--- public/js/sessionData.js
+++ public/js/sessionData.js
@@ -8,32 +8,20 @@ function cleanImageNames(inputArray){
     for(i= 0; i < inputArray.length; i++){
         imageNames[i] = inputArray[i].split('/')[4]
     }
-     console.log(imageNames);
+    //  console.log(imageNames);
 }
 
 // this gets called in conductor.html when a figure is sent.
 // "input" is expecting an object called whoWhom in conductor.html
 
 function sessionUpdateData(input){
-
-    // create a new timestamp
     var now = new Date();
-    var time = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
-
-    // need to shallow clone object so it's not by reference
-    var objClone = Object.assign({},input)
-
-    // this loop was copied from similar code in conductor.html
-
-    for (i=0; i < Object.keys(objClone).length; i++){
-
-          //slicing off the number of the image from whoWhat and using it as an index to the filename array.
-          objClone['player' + (i+1)] = imageNames[objClone['player' + (i+1)].slice(2)];  
-    };
-
-     // adds a time stamp and then shows the current state of all performers.ç
-     sessionData.push(time,objClone);
-
+    var time = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();  // create a new timestamp
+    var objClone = Object.assign({},input) // need to shallow clone object so it's not by reference
+        for (i=0; i < Object.keys(objClone).length; i++){  // this loop was copied from similar code in conductor.html
+            objClone['player' + (i+1)] = imageNames[objClone['player' + (i+1)].slice(2)];  //slicing off the number of the image from whoWhat and using it as an index to the filename array.
+        };
+     sessionData.push(time,objClone); // adds a time stamp and then shows the current state of all performers
      // ^ this "sessionData" array can be sent to database or text file for recording
 }
 
